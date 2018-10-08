dca4bf7c9aa7cdf55777186461dc8443188315e5
Ray Liu <liuxi09@ca.com>
2018-10-08 13:59:23 -0700

    JWT Securty part 2 done

--- src/main/java/com/erminray/polls/config/AuditingConfig.java
+++ src/main/java/com/erminray/polls/config/AuditingConfig.java
@@ -1,4 +1,4 @@
-package com.erminray.polls.model.config;
+package com.erminray.polls.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
